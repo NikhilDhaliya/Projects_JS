123d88176cfd6cd0091c3d876acbaa783befd21c
Nikhil Dhaliya <nikhil11754@gmail.com>
2025-04-13 11:36:39 +0530

    Update

---
+++ Image-Gallery/gallery-start/main.js
@@ -0,0 +1,43 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+const fileNames = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg","pic1 copy.jpg"];
+
+/* Declaring the alternative text for each image file */
+
+const alternativeText = {
+    "pic1.jpg" : "Eye",
+    "pic2.jpg" : "cloud",
+    "pic3.jpg" : "flowers",
+    "pic4.jpg" : "ancient",
+    "pic5.jpg" : "butterfly",
+    "pic1 copy.jpg" : "Eye copy"
+}
+
+/* Looping through images */
+
+for (let img of fileNames) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${img}`);
+    newImage.setAttribute('alt', alternativeText[img] || 'Gallery image');
+    thumbBar.appendChild(newImage);
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener("click",darken)
+
+function darken(){
+    if(overlay.style.opacity === "0.1"){
+        overlay.style.opacity = "0.7";
+    }
+    else{
+        overlay.style.opacity = "0.1";
+    }
+    
+}
